334302a8e5d15b57a15337fa5f5d01f745c7c44c
jarryd smyth <jarryd@myconnect.com.au>
2021-01-14 18:46:28 +1100

    stupid coaching

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+  def question
+    
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = ""
+    if @question == "I am going to work"
+      @answer = "great!"
+    elsif @question[-1] == '?'
+      @answer = "Silly question, get dressed and go to work!"
+    else 
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
